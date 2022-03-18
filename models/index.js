d1a3b3be2fcfe8067c82ef3198b3665a53413169
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-03-18 20:55:50 +1030

    added picnotes and tripnotes to models

--- models/index.js
+++ models/index.js
@@ -4,6 +4,9 @@ const Trip = require('./Trip');
 const Itinerary = require('./Itinerary');
 const Item = require('./Item');
 const UserTrip = require('./UserTrip');
+const TripNote = require('./TripNote');
+const PicNote = require('./PicNote');
+
 
 //User has many Trips through UserTrip
 User.belongsToMany(Trip, {through: UserTrip, foreignKey: 'user_id'});
@@ -14,6 +17,12 @@ User.hasMany(Picture, {foreignKey: 'user_id'});
 //User has many Pictures
 User.hasMany(Item, {foreignKey: 'user_id'});
 
+//User has many TripNotes
+User.hasMany(TripNote, {foreignKey: 'user_id'});
+
+//User has many PicNotes
+User.hasMany(PicNote, {foreignKey: 'user_id'});
+
 //Trip has many users through UserTrip
 Trip.belongsToMany(User, {through: UserTrip, foreignKey: 'trip_id'});
 
@@ -22,6 +31,12 @@ Trip.hasOne(Itinerary, {foreignKey: 'trip_id'});
 
 //Trip has many Pictures
 Trip.hasMany(Picture, {foreignKey: 'trip_id'});
+
+//Trip has many TripNotes
+Trip.hasMany(TripNote, {foreignKey: 'trip_id'});
+
+//Picture has many PicNotes
+Picture.hasMany(PicNote, {foreignKey: 'trip_id'});
 
 //Itinerary has many items
 Itinerary.hasMany(Item, {foreignKey: 'Itinerary_id', onDelete: 'CASCADE'});
