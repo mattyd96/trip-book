1ec5268f31941ccf05bb4010661d7398f6b454ac
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-03-24 20:17:25 +1030

    added in session functionality

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const models = require('../models');
 const authMiddleware = require("../middlewares/auth")
 
 /* GET home page. */
-router.get('/', authMiddleware.authorize, function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', /*authMiddleware.authorize,*/ function (req, res) {
+  res.render('index', { isLoggedIn: true });
 });
 
 module.exports = router;
