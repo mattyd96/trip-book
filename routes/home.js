b9f81e0e4dd5051143ee8bb172c148bd83df1fdb
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-03-24 23:38:26 +1030

    fixed errors with kanban rendering

--- routes/home.js
+++ routes/home.js
@@ -7,4 +7,9 @@ const authMiddleware = require("../middlewares/auth")
 /* GET home page. */
 router.get('/', /*authMiddleware.authorize,*/ renderHome);
 
+
+
+// test route to see if kanban will render correctly
+router.get('/kanban', (req, res) => {res.render('kanban', {style: 'kanban'})});
+
 module.exports = router;
