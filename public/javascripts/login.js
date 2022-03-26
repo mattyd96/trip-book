5bf23871f1092fa173627447aa0ffe48dc1a66d7
Matthew <26681440+mattyd96@users.noreply.github.com>
2022-03-26 18:33:59 +1030

    changed file logout function is located in so all pages have access to it and navbar works correctly

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -115,20 +115,6 @@ const login = async (event) => {
   }
 };
 
-const logout = async () => {
-  // logout
-  const response = await fetch('/users/logout', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json'}
-  });
-  // go to home on success
-  if (response.ok) {
-    document.location.assign('/');
-  } else {
-    console.log('error when logging out');
-  }
-}
-
 if(signupForm) {signupForm.addEventListener("submit", signup);}
 if(loginForm) {loginForm.addEventListener("submit", login);}
 console.log('loaded');
